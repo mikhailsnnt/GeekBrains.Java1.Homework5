ffb0b759d2d878161fad4316a6a4cbf1508a0a85
Mihail Nikitin <nitinin.miha@icloud.com>
2021-07-23 23:40:27 +0300

    All task's done.

--- src/Homework5.java
+++ src/Homework5.java
@@ -1,5 +1,23 @@
 public class Homework5 {
     public static void main(String[] args) {
-
+        Employee[] employees = {
+                new Employee("Ivan Petrovich", "PE Teacher","email@mail.com","+7123456789",
+                        10,41 ),
+                new Employee("Cherniy Kot", "Primus pochinaet","email@mail.com","+7123456789",
+                        2,10000000 ),
+                new Employee("Mika Karpovich", "Engineer","email@mail.com","+7123456789",
+                        10,30 ),
+                new Employee("Ustal pridumivat", "PE Teacher","email@mail.com","+7123456789",
+                10,40 ),
+                new Employee("Ustal Natanovich", "PE Teacher","email@mail.com","+7123456789",
+                        10,20 ),
+                new Employee("Ivan Ivanovich", "PE Teacher","email@mail.com","+7123456789",
+                        10,10 ),
+        };
+        for (Employee employee:
+             employees) {
+            if (employee.getAge() >40)
+                employee.printInfo();
+        }
     }
 }
